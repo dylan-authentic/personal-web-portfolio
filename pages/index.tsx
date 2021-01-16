48eb6cddaa2d780c1b2855422ad25d8e4c53b041
Dylan Authentic <dylan@authentic.tech>
2021-01-15 19:01:40 -0500

    Updated Homepage Styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
-import styles from '../styles/Home.module.css';
+import Plants from '../components/plants';
+import styles from '../styles/Main.module.css';
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
       </Head>
       <div className={styles.section}>
         <div className={styles.introHeader}>
-          <h1>☕️ dylan.cafe</h1>
+          <h1><span className={styles.coffeeMug}>☕️</span> dylan.cafe</h1>
           <div>
             <Link href="/">
               <a className={styles.menulink}>Home</a>
@@ -30,8 +31,8 @@ export default function Home() {
         <div className={styles.introContent}>
           <p>
             I'm laser-focused when it comes to my work, determined to win when it comes to my goals, & good-hearted when it comes to treating others.‍
-            <br/><br/>I'm an entrepreneur building businesses in my spare time, and I advise small and medium-sized businesses in adopting simple and useful technology for the digital era.‍
             <br/><br/>I'm a computer programmer with a background in fullstack web development, database management, & cloud-native applications.
+            <br/><br/>I'm an entrepreneur building businesses in my spare time, and I currently advise small and medium-sized businesses in adopting simple and useful technology for the digital era.‍
           </p>
         </div>
       </div>
